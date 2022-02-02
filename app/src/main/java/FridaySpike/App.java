dc40e75eafa4cf2acc7f1bebbce2c0ee14d475eb
Izabela Kulakowska <ikulakowska@microsoft.com>
2022-02-02 16:49:49 +0100

    Add changes

--- app/src/main/java/FridaySpike/App.java
+++ app/src/main/java/FridaySpike/App.java
@@ -8,7 +8,12 @@ public class App {
         return "Hello World!";
     }
 
+    public String getAnotherGreeting() {
+        return "Greetings!";
+    }
+
     public static void main(String[] args) {
         System.out.println(new App().getGreeting());
+        System.out.println(new App().getAnotherGreeting());
     }
 }
